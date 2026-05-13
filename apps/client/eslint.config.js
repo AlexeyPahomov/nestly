65f104844417760e12295c49d5ae6ca3141a4251
Алексей Пахомов <a.pahomov@mkskom.ru>
2026-05-13 11:41:38 +0300

    chore: tailwind

--- apps/client/eslint.config.js
+++ apps/client/eslint.config.js
@@ -1,9 +1,14 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+
+const tsconfigRootDir = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -16,6 +21,9 @@ export default defineConfig([
       reactRefresh.configs.vite,
     ],
     languageOptions: {
+      parserOptions: {
+        tsconfigRootDir,
+      },
       globals: globals.browser,
     },
   },
