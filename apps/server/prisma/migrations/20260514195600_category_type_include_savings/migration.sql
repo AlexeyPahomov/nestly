e246dda2bd1b72f2a875490019c6fa2d74802700
Алексей Пахомов <a.pahomov@mkskom.ru>
2026-05-14 20:04:23 +0300

    chore: миграция бд

---
+++ apps/server/prisma/migrations/20260514195600_category_type_include_savings/migration.sql
@@ -0,0 +1,4 @@
+-- Align with @nestly/shared CATEGORY_TYPES: income | expense | savings
+ALTER TABLE "Category" DROP CONSTRAINT IF EXISTS "Category_type_check";
+
+ALTER TABLE "Category" ADD CONSTRAINT "Category_type_check" CHECK (type IN ('income', 'expense', 'savings'));
